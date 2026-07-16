386daf1fe3f436d608b93c303db2df4b1a730023
root <root@6559303-gg070737.twc1.net>
2026-07-16 05:17:07 +0000

    fix: properly define qa_bp Blueprint

--- app/qa/routes.py
+++ app/qa/routes.py
@@ -5,7 +5,8 @@ from flask import render_template, request, jsonify, flash, redirect, url_for, c
 from flask_login import login_required, current_user
 from core.db_models import db, QATestCase, QAReport, QAReportHistory
 from app.auth.rbac_utils import require_role
-# # from . import qa_bp
+from flask import Blueprint
+qa_bp = Blueprint("qa", __name__)
 
 QA_ROLES = ('tester', 'chief_tester', 'admin', 'creator', 'chief_admin')
 QA_AREAS = [
